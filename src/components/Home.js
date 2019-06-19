e997f3e7347a754b44a9259da1957afe33e8fdaa
Yahil Madakiya <yahil.madakiya@rtcamp.com>
2019-06-19 12:12:25 +0530

    Change import statement as per updated folder structure

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,8 @@
 import React from "react";
 
 // Import Components
-import Header from "../components/Header/Header";
-import Posts from "../components/Posts/Posts";
+import Header from "./Header/Header";
+import Posts from "./Posts/Posts";
 
 const Home = () => {
 	return (
