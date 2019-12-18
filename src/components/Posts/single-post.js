24a92348ffbf7d80490f0567f9b3ae198ed11423
Yahil Madakiya <yahil.madakiya@rtcamp.com>
2019-12-18 14:44:09 +0530

    Add more elements to single page

--- src/components/Posts/single-post.js
+++ src/components/Posts/single-post.js
@@ -4,6 +4,7 @@ import { gql } from 'apollo-boost';
 
 class SinglePost extends Component {
 
+
     render() {
         const data = this.props.data;
 
@@ -12,9 +13,17 @@ class SinglePost extends Component {
         }
 
         return (
-            <article className="container">
-                <img src={data.postBy.featuredImage.mediaItemUrl} />
-                {data.postBy.title}
+            <article className="article">
+                <img
+                    className="article-image"
+                    alt={data.postBy.featuredImage.title}
+                    src={data.postBy.featuredImage.sourceUrl}
+                    srcSet={data.postBy.featuredImage.srcSet}
+                />
+                <div className="container">
+                    <h1 className="article-title">{data.postBy.title}</h1>
+                    <div dangerouslySetInnerHTML={{ __html: data.postBy.content}}></div>
+                </div>
             </article>
         );
     }
