3daed06692dabd0aa6b465f96ba490c30b23dc68
Yahil Madakiya <yahil.madakiya@rtcamp.com>
2019-12-18 09:46:28 +0530

    Use API link from config file

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import config from './client-config';
 
 import { ApolloProvider } from "react-apollo";
 import ApolloClient from "apollo-boost";
 
 // apollo client setup
 const client = new ApolloClient({
-	uri: "http://site.local/graphql"
+	uri: config.graphqlUrl
 });
 
 ReactDOM.render(
