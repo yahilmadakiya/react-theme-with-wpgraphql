59ec18c67dc7d8747204b53760865f46e006c8ad
Yahil Madakiya <yahil.madakiya@rtcamp.com>
2019-12-18 21:50:19 +0530

    Add author page template

---
+++ src/pages/Author.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../components/header/Header';
+import SingleAuthor from '../components/authors/SingleAuthor';
+
+const Author = () => {
+	return(
+		<React.Fragment>
+			<Header />
+			<SingleAuthor />
+		</React.Fragment>
+	);
+}
+
+export default Author;
