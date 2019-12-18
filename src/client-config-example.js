a5c6257a00cf727bb45d4c2708e660c3d1a6b110
Yahil Madakiya <yahil.madakiya@rtcamp.com>
2019-12-18 09:46:04 +0530

    Add client-config-example

---
+++ src/client-config-example.js
@@ -0,0 +1,11 @@
+/**
+ * Contains client side configuration options.
+ */
+
+const config = {
+    siteUrl: 'http://localhost:3000',
+    graphqlUrl: 'http://localhost:8080/graphql',
+    postCodeLocale: 'IN'
+};
+
+export default config;
