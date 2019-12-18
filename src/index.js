862e41e62906aef0ab915529a42ab87c8395477d
Yahil Madakiya <yahil.madakiya@rtcamp.com>
2019-12-18 10:02:35 +0530

    Add ApolloClient setup file

--- src/index.js
+++ src/index.js
@@ -1,20 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import config from './client-config';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ApolloProvider } from 'react-apollo';
+import client from './ApolloClient';
 
-import { ApolloProvider } from "react-apollo";
-import ApolloClient from "apollo-boost";
-
-// apollo client setup
-const client = new ApolloClient({
-	uri: config.graphqlUrl
-});
+import App from './App';
+import './index.css';
 
 ReactDOM.render(
 	<ApolloProvider client={client}>
 		<App />
 	</ApolloProvider>,
-	document.getElementById("root")
+	document.getElementById( 'root' )
 );
